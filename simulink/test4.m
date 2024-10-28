8dac02870cb1194f8ff840b69a63865caf3d4fb6
Marcos <marcoolivera096@gmail.com>
2024-10-28 16:28:31 -0300

    updates

--- simulink/test4.m
+++ simulink/test4.m
@@ -2,7 +2,19 @@ pkg load control
 
 # Definir a função de transferência de um sistema de segunda ordem
 s = tf('s');
-G = (s + 3) / (s^3 + 5*s^2 + 6*s + 4);  # Função de transferência mais complexa
+% % a)
+% G = (s + 2) / (s^4 + 2*s^3 + 3*s^2 + 4*s+ 5 );  # Função de transferência mais complexa
+
+%b)
+G = (s^2 + 3*s + 2) / (s^3 + 5*s^2 + 3*s+ 1 );  # Função de transferência mais complexa
+
+% % c)
+% G = (s + 2) / (s^4 + 2*s^3 + 3*s^2 + 4*s+ 5 );  # Função de transferência mais complexa% 
+
+
+% % d)
+% G = (s + 2) / (s^4 + 2*s^3 + 3*s^2 + 4*s+ 5 );  # Função de transferência mais complexa
+
 
 # Análise de Lugar das Raízes (Root Locus)
 figure;
